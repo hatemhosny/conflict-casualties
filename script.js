0f4f9143f632e98ee6c0b14001dc03b0deee2a0b
Hatem Hosny <hatemhosny@users.noreply.github.com>
2024-01-27 05:50:41 +0200

    deploy

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-import { race } from "./racing-bars.js";
+import { loadData, race } from 'racing-bars';
 
 const options = {
-  dataType: "csv",
-  dataShape: "wide",
-  title: "Deaths in Israeli/Palestinian conflicts",
-  subTitle: "",
-  caption: "Source: OCHA",
-  dateCounter: "YYYY",
-  showIcons: true,
-  labelsPosition: "outside",
-  colorMap: {
-    Palestinians: "#1abb57",
-    Israelis: "#2f62d8",
-  },
+  selector: '#race',
+  dataShape: 'wide',
+  title: 'Palestinian Casualties',
+  subTitle: 'data.techforpalestine.org',
+  dateCounter: 'DD MMM YYYY',
+  tickDuration: 500,
+  labelsPosition: 'outside',
   fixedScale: true,
-  topN: 3,
-  dataTransform: (data) => data.map((d) => ({
-    ...d,
-    date: d.date + '-01-02',
-  })),
+  fixedOrder: ['Injured', 'Killed', 'Children Killed', 'Women Killed', 'Medicals Killed', 'Press Killed', ''],
+  colorMap: {
+    'Injured': '#b94d4d',
+    'Killed': '#000',
+    'Children Killed': '#4db9b2',
+    'Women Killed': '#a9a9a9',
+    'Medicals Killed': '#ba0603',
+    'Press Killed': '#1858bf',
+  }
 };
-race("./data.csv", "#race", options);
+
+loadData('https://data.techforpalestine.org/api/v2/casualties_daily.min.json').then((data) => {
+  data = data.map((d) => ({
+    date: d.report_date,
+    "Injured": d.ext_injured_cum,
+    "Killed": d.ext_killed_cum,
+    "Children Killed": d.ext_killed_children_cum,
+    "Women Killed": d.ext_killed_women_cum,
+    "Medicals Killed": d.ext_med_killed_cum,
+    "Press Killed": d.ext_press_killed_cum,
+  }));
+  race(data, options);
+});
